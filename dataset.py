6ab3136c0b2b165d66f391289561bbd9bf53856c
Shashank Manjunath <manjunath.sh@northeastern.edu>
2024-07-11 16:03:44 -0400

    Finished SQI calculation

--- dataset.py
+++ dataset.py
@@ -14,6 +14,103 @@ import mne
 import tda_utils
 
 
+class EpochItem:
+    def __init__(self, epoch, t_start, t_len):
+        self.epoch = epoch
+        self.t_start = t_start
+        self.t_len = t_len
+
+        # Slack time
+        self.t_slack = 1.0
+
+    def overlaps(self, other) -> bool:
+        # Allow t_slack difference between old end time and new start time
+        end_time = self.t_start + self.t_len
+        slack_time = end_time + self.t_slack
+
+        if slack_time < other.t_start:
+            return False
+        else:
+            return True
+
+
+class EpochCache:
+    def __init__(self, n_epochs: int, sampling_freq: float):
+        self.n_epochs = n_epochs
+        self.sampling_freq = sampling_freq
+        self.freq_spread = 4
+
+        self.bp_lo_freq = 0.1
+        self.bp_hi_freq = 0.75
+
+        self.epoch_cache = []
+        self.sos_butter_bandpass = scipy.signal.butter(
+            N=3,
+            Wn=[self.bp_lo_freq, self.bp_hi_freq],
+            btype="bandpass",
+            output="sos",
+            fs=self.sampling_freq,
+        )
+
+    def add_epoch(self, epoch: np.ndarray, t_start: float, t_len: float) -> None:
+        if len(self.epoch_cache) == 0:
+            # No epochs in cache; add to cache
+            self.epoch_cache.append(EpochItem(epoch, t_start, t_len))
+        else:
+            cur_epoch = EpochItem(epoch, t_start, t_len)
+            last_epoch = self.epoch_cache[-1]
+
+            if last_epoch.overlaps(cur_epoch):
+                # New epoch is consecutive to previous epoch
+                if len(self.epoch_cache) == self.n_epochs:
+                    # Pop out first item if adding current will make more than
+                    # the maximum number of epochs
+                    self.epoch_cache = self.epoch_cache[1:]
+                elif len(self.epoch_cache) > self.n_epochs:
+                    raise RuntimeError("SQI Epoch Cache too large!")
+                # Add epoch to end of cache
+                self.epoch_cache.append(cur_epoch)
+            else:
+                # New epoch is not consecutive to previous epoch, so we clear
+                # cache and restart
+                self.epoch_cache = []
+                self.epoch_cache.append(cur_epoch)
+
+    def get_epoch_sequence(self) -> np.ndarray:
+        if len(self.epoch_cache) < 5:
+            raise RuntimeError("Insufficient data in epoch cache!")
+        epoch_data = np.concatenate([x.epoch for x in self.epoch_cache], axis=-1)
+        return epoch_data
+
+    def get_sqi(self) -> float:
+        if len(self.epoch_cache) < 5:
+            return -1.0
+        epoch_data = np.concatenate([x.epoch for x in self.epoch_cache], axis=-1)
+
+        # Bandpass signal
+        bandpass_signal = scipy.signal.sosfilt(self.sos_butter_bandpass, epoch_data)
+
+        # Fourier transform
+        fft_arr = np.abs(scipy.fft.fft(bandpass_signal)).squeeze()
+        fft_arr = np.power(fft_arr, 2)
+        ts = 1.0 / self.sampling_freq
+        freqs = scipy.fft.fftfreq(fft_arr.shape[-1], d=ts)
+
+        # Finding peak in spectrum
+        valid_freqs = (freqs >= self.bp_lo_freq) & (freqs <= self.bp_hi_freq)
+        fft_arr = fft_arr[valid_freqs]
+        freqs = freqs[valid_freqs]
+        max_freq_idx = fft_arr.argmax()
+
+        # Calculating SQI
+        signal_power = fft_arr.sum()
+        low_idx = max_freq_idx - self.freq_spread
+        hi_idx = max_freq_idx + self.freq_spread
+        maxpow_band_power = fft_arr[low_idx:hi_idx].sum()
+        sqi = maxpow_band_power / signal_power
+        return sqi
+
+
 class AirflowSignalProcessor:
     def __init__(self, pt_id: str, data_dir: str):
         self.pt_id = pt_id
@@ -39,6 +136,7 @@ class AirflowSignalProcessor:
 
         self.d = 3
         self.target_resamp_rate = 4
+        self.n_epochs_sqi = 6
 
         if not os.path.exists(self.edf_fname):
             raise RuntimeError("EDF file for patient id not found!")
@@ -82,17 +180,36 @@ class AirflowSignalProcessor:
         n_rows = target_intervals.shape[0]
 
         data = []
+
+        # Running epochs for signal quality index calculation
+        epoch_cache = EpochCache(self.n_epochs_sqi, sampling_freq=sfreq)
+
         pbar = tqdm(target_intervals.iterrows(), total=n_rows)
         for idx, (_, interval) in enumerate(pbar):
             interval_start_idx = int(interval.onset * sfreq)
             interval_end_idx = int(interval.end * sfreq)
 
             # Getting airflow signal
-            data_arr = raw_edf.get_data(
+            cur_epoch = raw_edf.get_data(
                 picks=["Resp Airflow"],
                 start=interval_start_idx,
                 stop=interval_end_idx,
             )
+
+            epoch_cache.add_epoch(
+                cur_epoch,
+                t_start=interval["onset"],
+                t_len=interval["duration"],
+            )
+
+            sqi = epoch_cache.get_sqi()
+
+            if sqi < 0.25:
+                print(f"Skipping {idx}, SQI: {sqi}")
+                # SQI is too low, reject epoch
+                continue
+
+            data_arr = epoch_cache.get_epoch_sequence()
 
             # Calculate IRR signal
             irr_signal = self.get_irr(data_arr, sfreq)
@@ -101,8 +218,8 @@ class AirflowSignalProcessor:
             sublevel_dgms_irr = self.sublevel_set_filtration(irr_signal)
 
             # Skipping if we have very few points in filtration
-            #  if sublevel_dgms_irr[0].shape[0] < 2:
-            #      continue
+            if np.isnan(sublevel_dgms_irr[0]).sum() > 0:
+                continue
 
             ps_irr = self.persistence_summary(sublevel_dgms_irr[0])
             hepc_irr = self.hepc(sublevel_dgms_irr[0])
@@ -133,8 +250,14 @@ class AirflowSignalProcessor:
     def get_irr(self, arr: np.ndarray, sampling_freq: float) -> np.ndarray:
         #  clean_arr = nk.rsp_clean(arr.squeeze(), sampling_rate=sampling_freq)
         clean_arr = self.clean_rsp_signal(arr.squeeze(), sampling_rate=sampling_freq)
-        _, peaks = nk.rsp_peaks(clean_arr)
-        rsp_rate = nk.rsp_rate(clean_arr, peaks, sampling_rate=sampling_freq)
+        _, peaks = nk.rsp_peaks(
+            clean_arr,
+            sampling_rate=sampling_freq,
+            method="biosppy",
+        )
+        rsp_rate = nk.rsp_rate(
+            clean_arr, peaks, method="troughs", sampling_rate=sampling_freq
+        )
 
         T = arr.shape[-1] / sampling_freq
         n_samp = int(T * self.target_resamp_rate)
@@ -240,7 +363,18 @@ class AirflowSignalProcessor:
         return hepc_feat
 
 
-if __name__ == "__main__":
-    pt_id = "10000_17728"
-    loader = AirflowSignalProcessor(pt_id=pt_id, data_dir="/work/thesathlab/nchsdb/")
+def process_idx(idx):
+    data_dir = "/work/thesathlab/nchsdb/"
+
+    pt_ids = []
+    for pt_file in os.listdir(os.path.join(data_dir, "sleep_data")):
+        if pt_file.endswith(".edf"):
+            pt_ids.append(pt_file.replace(".edf", ""))
+
+    pt_id = pt_ids[idx]
+    loader = AirflowSignalProcessor(pt_id=pt_id, data_dir=data_dir)
     loader.process()
+
+
+if __name__ == "__main__":
+    process_idx(0)
